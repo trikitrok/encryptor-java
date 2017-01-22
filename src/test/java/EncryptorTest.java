201620532bdb96067f8f82b1359d12c1898c8389
trikitrok <trikitrok@gmail.com>
2017-01-22 23:49:28 +0100

    initial commit

---
+++ src/test/java/EncryptorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EncryptorTest {
+    @Test
+    public void it_just_checks_dependencies_are_working_find_delete_it() {
+        assertThat(false, is(true));
+    }
+}
